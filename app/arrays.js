680d7879f5f9606da57c08a5a0ef7d4005a19538
David Tan <davidtansf@gmail.com>
2015-05-12 10:47:02 -0700

    update solution for best practices

--- app/arrays.js
+++ app/arrays.js
@@ -4,14 +4,35 @@ define(function() {
   return {
     indexOf : function(arr, item) {
 
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === item) {
+            return i;
+        }
+      }
+      return -1;
     },
 
     sum : function(arr) {
+
+      var sum = 0;
+
+      for (var i = 0; i < arr.length; i++) {
+        sum += arr[i];
+      }
+
+      return sum;
 
     },
 
     remove : function(arr, item) {
 
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === item) {
+            arr.splice(i,1);
+            i--; // index changes, need to increment
+        }
+      }
+      return arr;
     },
 
     removeWithoutCopy : function(arr, item) {
